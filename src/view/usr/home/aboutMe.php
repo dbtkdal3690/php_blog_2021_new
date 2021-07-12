fd3e46ce80a7b016bd043988afe0411b4e9e69ba
dbtkdal3690 <dbtkdal1590@gmail.com>
2021-07-12 13:38:58 +0900

    나의 페이지 소개

--- src/view/usr/home/aboutMe.php
+++ src/view/usr/home/aboutMe.php
@@ -8,8 +8,12 @@ $pageTitle = "주인장소개";
 <section class="section-about-me">
   <div class="container mx-auto">
     <div class="con-pad">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, quasi atque tempora beatae dicta odit sequi nostrum at assumenda quibusdam illum neque tenetur fuga facilis dolorum recusandae ad molestias temporibus.
-    </div>
+      <hr>
+      이름 : 유상미 <br>
+      생년월일 : 1999.01.27<br>
+      소개글 : 개발중입니다.<br>
+    
   </div>
 </section>
 <?php require_once __DIR__ . "/../foot.php"; ?>
+ 
